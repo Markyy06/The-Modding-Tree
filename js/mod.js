7e2c4eeb34d9c046d4c44b6ab610f4c033ddb96b
Markyy06 <167579065+Markyy06@users.noreply.github.com>
2024-04-19 22:30:18 +0200

    Updated mod.js
    - Changed the Name to "The Meme Tree"
    - Changed the id to "MemeTree"
    - Changed the author to "Markyy06"
    - Changed the Currency Name to "Memes"

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Meme Tree",
+	id: "MemeTree",
+	author: "Markyy06",
+	pointsName: "Memes",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
